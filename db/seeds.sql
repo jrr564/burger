f135553e153306a4f0086f622084630111b89138
admin <admin@vraney-mbp.thelamplighterschool.org>
2018-02-14 12:16:21 -0600

    closer

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("BBQ & Grilled Onions");
+
+INSERT INTO burgers (burger_name) VALUES ("Veggie");
